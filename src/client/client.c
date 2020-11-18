715909d1d5abcdc0913f16a33e131b21ee757c3c
leoM26 <leoalbouy26@gmail.com>
2020-11-18 17:01:05 +0100

    glade begin

--- src/client/client.c
+++ src/client/client.c
@@ -19,14 +19,28 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <stdbool.h>
+#include<gtk/gtk.h>
 
 #include "clientcxnmanager.h"
-
+GtkBuilder *builder = NULL;
 /*
  * 
  */
+
+
+
 int main(int argc, char **argv)
 {
+
+
+    GtkWidget *win;
+
+    gtk_init(&argc, &argv);
+    builder = gtk_builder_new_from_file("glade/Interface.glade");
+    win = GTK_WIDGET(gtk_builder_get_object(builder, "app_win"));
+    gtk_builder_connect_signals(builder, NULL);
+    gtk_widget_show(win);
+    gtk_main();
 
     Config cfg;
     read_config(&cfg, "client_config.cfg");
